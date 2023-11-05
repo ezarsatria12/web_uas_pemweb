b9d987bf6530d7d37d00d40d8cf6d252b8182696
Ezarsatria12 <Ezar3004>
2023-11-06 00:32:06 +0700

    1st

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,78 +1,141 @@
 import ReactDOM from "react-dom";
-import { Dialog } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import "../index";
 import Navbar from "./component/nav";
 import Footer from "./component/footer";
-import { useState} from "react";
+import {
+  useState,
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  Slider,
+} from "react";
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className="bg-white">
-      <Navbar />
-
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="hero relative isolate min-h-screen">
+        <Navbar />
         <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="absolute bottom-0 left-0 overflow-hidden mix-blend-normal"
           aria-hidden="true"
         >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
+          <img
+            className="absolute h-72 w-auto  inset-x-0 top-35 object-cover opacity-5 -z-10"
+            src={process.env.PUBLIC_URL + "/assets/img/logo2.png"}
+            alt=""
+          />
+          <img
+            className="w-screen object-cover -z-40"
+            src={process.env.PUBLIC_URL + "/assets/img/bghero3.png"}
+            alt=""
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
+        <div className="relative h-100 flex flex-col justify-center  gap-y-6  px-10 lg:px-20">
+          <h1 className="max-w-screen-sm text-xxl font-porsche font-semibold tracking-wide">
+            The Thrill of Driving Perfection
+          </h1>
+          <button
+            className="w-fit px-6 py-2 outline outline-offset-2 outline-2 text-base font-porsche font-semibold"
+            type="button"
+          >
+            The 911. Starting at $90,900
+          </button>
+        </div>
+      </div>
+      <div className="relative isolate px-10 lg:px-20">
+        <div className="cardcontainer flex justify-between gap-4 sm:gap-8 py-4 sm:py-8">
+          <div className="relative sm:w-1/2 md:w-1/3 lg:w-1/4 overflow-hidden h-auto">
+            <img
+              className="object-cover w-full"
+              src={process.env.PUBLIC_URL + "/assets/img/news1.jpg"}
+              alt="Porsche 911 Le Mans"
+            />
+            <h3 className="absolute bottom-0 left-0 px-4 py-2 sm:px-6 sm:py-4 bg-black text-white">
+              Porsche 911 Le Mans
+            </h3>
           </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+          <div className="relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 overflow-hidden">
+            <img
+              className="object-cover w-full"
+              src={process.env.PUBLIC_URL + "/assets/img/news1.jpg"}
+              alt="Porsche 911 Le Mans"
+            />
+            <h3 className="absolute bottom-0 left-0 px-4 py-2 sm:px-6 sm:py-4 bg-black text-white">
+              Porsche 911 Le Mans
+            </h3>
+          </div>
+          <div className="relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 overflow-hidden">
+            <img
+              className="object-cover w-full"
+              src={process.env.PUBLIC_URL + "/assets/img/news1.jpg"}
+              alt="Porsche 911 Le Mans"
+            />
+            <h3 className="absolute bottom-0 left-0 px-4 py-2 sm:px-6 sm:py-4 bg-black text-white">
+              Porsche 911 Le Mans
+            </h3>
+          </div>
+        </div>
+        <div className="flex justify-between bg-black rounded-md">
+          <div className="textbox flex flex-col px-16 justify-center">
+            <h3 className="text-xl font-porsche text-white">
+              Find Your Porsche Center
+            </h3>
+            <p className="font-porsche text-white">
+              A Porsche Center, and your dream Porsche vehicle, may be closer
+              than you think. Search our Porsche Center network for the location
+              closest to you.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
+            <button
+              className="w-fit px-6 py-2 my-6 bg-white text-base font-porsche font-semibold"
+              type="button"
+            >
+              Search now
+            </button>
           </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
+          <img
+            className=""
+            src={process.env.PUBLIC_URL + "/assets/img/infodealercenter2.png"}
           />
         </div>
+        <div className="flex flex-col my-40">
+          <h2 className="text-center text-xl font-porsche font-semibold">
+            Discover
+          </h2>
+          <div className="cardcontainer flex gap-12 py-8 ">
+            <a href="/" className="relative overflow-hidden h-auto">
+              <img
+                className="object-cover"
+                src={process.env.PUBLIC_URL + "/assets/img/news1.jpg"}
+              />
+              <h3 className="absolute top-0 left-0 px-6 py-4">
+                Porsche 911 Le Mans
+              </h3>
+            </a>
+            <a href="/" className="relative overflow-hidden">
+              <img
+                className="object-cover"
+                src={process.env.PUBLIC_URL + "/assets/img/news1.jpg"}
+              />
+              <h3 className="absolute top-0 left-0 px-6 py-4">
+                Porsche 911 Le Mans
+              </h3>
+            </a>
+            <a href="/" className="relative overflow-hidden">
+              <img
+                className="object-cover"
+                src={process.env.PUBLIC_URL + "/assets/img/news1.jpg"}
+              />
+              <h3 className="absolute top-0 left-0 px-6 py-4">
+                Porsche 911 Le Mans
+              </h3>
+            </a>
           </div>
-          <Footer/>
+        </div>
+      </div>
     </div>
   );
-};
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  extend: {
+    spacing: {},
   },
   plugins: [],
+  theme: {
+    extend: {
+    fontSize: {
+      xxl: ["64px", "100.928px"],
+      xl: ["32px", "auto"],
+    },
+    fontFamily: {
+      porsche: "porschenext",
+    },
+    letterSpacing: {
+      normal: "0",
+      wide: "1.2px",
+      wider: ".05em",
+      widest: ".1em",
+      widest: ".25em",
+    },
+      height: {
+        100: "600px",
+        8: "24px",
+      },
+    },
+  },
 };
 
